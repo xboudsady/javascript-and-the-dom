124c818b1d7a7d8b5e98c37e03b7996aaf94687a
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-09 16:08:08 -0800

    Added input field

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
-const myList = document.getElementsByTagName('li');
+const input = document.querySelector('input');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button');
 
-for (let i = 0; i < myList.length; i += 1) {
-    myList[i].style.color = 'purple';
-}
-
-const errorNotPurple = document.querySelectorAll('.error-not-purple');
-
-for (let i = 0; i < errorNotPurple.length; i += 1) {
-    errorNotPurple[i].style.color = 'red';
-}
-
-const evens = document.querySelectorAll('li:nth-child(odd)');
-
-for (let i = 0; i < evens.length; i += 1) {
-    evens[i].style.backgroundColor = 'lightgray';
-}
+button.addEventListener('click', () => {
+    p.innerHTML = input.value + ':';
+});
