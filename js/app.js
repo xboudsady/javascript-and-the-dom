b2fc19ce2c1b06a47be7882f9c5fd804d4494e7b
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-10 21:32:52 -0800

    Added up and remove button functionality

--- js/app.js
+++ js/app.js
@@ -3,8 +3,28 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
+const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+
+listUl.addEventListener('click', (evt) => {
+    if (evt.target.tagName == 'BUTTON') {
+        if (evt.target.tagName == 'remove') {
+            let li = evt.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+        if (evt.target.className == 'up') {
+            let li = evt.target.parentNode;
+            let prevLi = li.previousElementSibling;
+            let ul = li.parentNode;
+            if (prevLi) {
+                ul.insertBefore(li, prevLi);
+            }
+        }
+    }
+});
+
 
 toggleList.addEventListener('click', () => {
     if (listDiv.style.display === 'none') {
@@ -18,9 +38,18 @@ toggleList.addEventListener('click', () => {
 
 descriptionButton.addEventListener('click', () => {
     descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
+});
+
+addItemButton.addEventListener('click', () => {
+        let ul = document.getElementsByTagName('ul')[0];
+        let li = document.createElement('li');
+        li.textContent = addItemInput.value;
+        ul.appendChild(li);
+        addItemInput.value = '';
 });
